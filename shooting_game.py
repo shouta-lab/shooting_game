c22a07ce4d7f390cdb34aa6a23230d53347a72d6
t <shoutarouuuu@gmail.com>
2022-06-26 18:11:23 +0900

    a

--- shooting_game.py
+++ shooting_game.py
@@ -22,6 +22,7 @@ tama_no = 0
 tama_f = [False]*TAMA_MAX
 tama_x = [0]*TAMA_MAX
 tama_y = [0]*TAMA_MAX
+player_img = pygame.image.load("png/player.png")
 
 def set_tama():
     global tama_no
@@ -34,7 +35,7 @@ def move_tama():
     for i in range(TAMA_MAX):
         if tama_f[i] == True:
             tama_y[i] = tama_y[i] - 36
-            scrn.blit(player_img, [tama_x[i], tama_y[i]])
+            screen.blit(player_img, [tama_x[i], tama_y[i]])
             if tama_y[i] < 0:
                     tama_f[i] = False
 
@@ -76,7 +77,6 @@ def main():
         elif idx == 2: # play
             screen.fill(WHITE)
             pygame.draw.rect(screen, RED, [p_x-p_s_x, p_y-p_s_y, p_s_x*2, p_s_y*2])
-            player_img = pygame.image.load("png/player.png")
             if key[pygame.K_RIGHT] == 1: # 移動入力
                 p_x = p_x + 10
             elif key[pygame.K_LEFT] == 1:
@@ -86,7 +86,10 @@ def main():
             elif key[pygame.K_DOWN] == 1:
                 p_y = p_y + 10
             if key[pygame.K_SPACE] == 1:
-                set_tama()
+                    tama_f[tama_no] = True
+                    tama_x[tama_no] = p_x
+                    tama_y[tama_no] = p_y
+                    tama_no = (tama_no+1)%TAMA_MAX
             if p_x < 0:
                 p_x = 0
             elif p_x > 1280:
@@ -96,7 +99,12 @@ def main():
             elif p_y > 720:
                 p_y = 720
             screen.blit(player_img, (p_x-p_s_x, p_y-p_s_y))
-            move_tama()
+            for i in range(TAMA_MAX):
+                if tama_f[i] == True:
+                    tama_y[i] = tama_y[i] - 36
+                    screen.blit(player_img, [tama_x[i], tama_y[i]])
+                    if tama_y[i] < 0:
+                            tama_f[i] = False
         pygame.display.update()
         clock.tick(60)
 
